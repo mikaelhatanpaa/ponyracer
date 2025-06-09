c419f5379f1b00e45e6f7dff4d1266e5a08351ac
Mikael <mikael.hatanpaa@gmail.com>
2025-06-09 23:45:56 +0200

    Added life-cycle hook to fetch from service
    Created service composable/hook useRaceService

--- src/views/Races.vue
+++ src/views/Races.vue
@@ -6,8 +6,9 @@
 
 <script lang="ts">
 import Race from '@/components/Race.vue';
+import { useRaceService } from '@/composables/RaceService';
 import { RaceModel } from '@/models/RaceModel';
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 
 export default defineComponent({
   name: 'Races',
@@ -15,32 +16,12 @@ export default defineComponent({
     Race
   },
   setup() {
-    const races: Array<RaceModel> = [
-      {
-        id: 12,
-        name: 'Paris',
-        ponies: [
-          { id: 1, name: 'Gentle Pie', color: 'YELLOW' },
-          { id: 2, name: 'Big Soda', color: 'ORANGE' },
-          { id: 3, name: 'Gentle Bottle', color: 'PURPLE' },
-          { id: 4, name: 'Superb Whiskey', color: 'GREEN' },
-          { id: 5, name: 'Fast Rainbow', color: 'BLUE' }
-        ],
-        startInstant: '2020-02-18T08:02:00Z'
-      },
-      {
-        id: 13,
-        name: 'Tokyo',
-        ponies: [
-          { id: 6, name: 'Fast Rainbow', color: 'BLUE' },
-          { id: 7, name: 'Gentle Castle', color: 'GREEN' },
-          { id: 8, name: 'Awesome Rock', color: 'PURPLE' },
-          { id: 9, name: 'Little Rainbow', color: 'YELLOW' },
-          { id: 10, name: 'Great Soda', color: 'ORANGE' }
-        ],
-        startInstant: '2020-02-18T08:03:00Z'
-      }
-    ];
+    const raceService = useRaceService();
+    const races = ref<Array<RaceModel>>([]);
+
+    onMounted(async () => {
+      races.value = await raceService.list();
+    });
 
     return { races };
   }
--- src/views/__tests__/Races.spec.ts
+++ src/views/__tests__/Races.spec.ts
@@ -1,10 +1,22 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import { flushPromises, mount } from '@vue/test-utils';
 import Races from '@/views/Races.vue';
 import Race from '@/components/Race.vue';
+import { RaceModel } from '@/models/RaceModel';
+
+const mockRaceService = {
+  list: vi.fn()
+};
+vi.mock('@/composables/RaceService', () => ({
+  useRaceService: () => mockRaceService
+}));
 
 describe('Races.vue', () => {
   test('should display every race name in a title', async () => {
+    mockRaceService.list.mockResolvedValue([
+      { name: 'London', startInstant: '2020-02-18T08:02:00Z' },
+      { name: 'New York', startInstant: '2020-02-18T08:03:00Z' }
+    ] as Array<RaceModel>);
     const wrapper = mount(Races);
     await flushPromises();
 
